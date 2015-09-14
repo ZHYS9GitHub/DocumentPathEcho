97a97ec8513bd6bd1e688114f452447a58c6e907
943890590 <943890590@qq.com>
2015-09-14 09:18:25 +0800

    Initial Commit

---
+++ DocumentPathEcho/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DocumentPathEcho
+//
+//  Created by 老师 on 15/9/14.
+//  Copyright (c) 2015年 echo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DocumentPathEcho/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DocumentPathEcho
+//
+//  Created by 老师 on 15/9/14.
+//  Copyright (c) 2015年 echo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
